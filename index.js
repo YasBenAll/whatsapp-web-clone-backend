3f8653c77bf3760d1a37d828fc0e33ef07ada674
YasBenAll <yassinbenallal@hotmail.com>
2023-01-01 22:58:57 +0100

    Implemented OpenAI for generating response

--- index.js
+++ index.js
@@ -1,8 +1,19 @@
+require('dotenv').config();
+const { Configuration, OpenAIApi} = require('openai');
+
 const express = require("express");
 const { getRandomSentence, getResponseInterval } = require("./utils");
 
 const PORT = process.env.PORT || 5000;
 const app = express();
+
+const configuration = new Configuration({
+    organization: "org-QU6yWcJ3d56TpA7To4UupLVb",
+    apiKey: process.env.NEXT_PUBLIC_OPENAI_API_KEY,
+});
+const openai = new OpenAIApi(configuration);
+
+
 const server = app.listen(PORT, () => console.log("Server running..."));
 
 const io = require("socket.io")(server, { cors: { origin: "*" } });
@@ -13,20 +24,36 @@ app.get("/", (req, res) => {
 });
 
 io.on("connection", (socket) => {
-	socket.on("fetch_response", (data) => {
-		const { userId } = data;
+	socket.on("fetch_response", async (data) => {
+		const { userId, message } = data;
+		console.log(message)
 		const responseInterval = getResponseInterval(1000, 4000);
 
-		setTimeout(() => {
-			socket.emit("start_typing", { userId });
+		const response = await openai.createCompletion({
+			model: "text-davinci-003",
+			prompt: `${message}`,
+			max_tokens: 10,
+			temperature: 0,
+		  });
+		console.log(response.data)
+	
+		if(response.data.choices[0].text) {
+			socket.emit("stop_typing", { userId });
+			socket.emit("fetch_response", {
+				response: response.data.choices[0].text,
+				userId,
+			});
+		}
 
-			setTimeout(() => {
-				socket.emit("stop_typing", { userId });
-				socket.emit("fetch_response", {
-					response: getRandomSentence(),
-					userId,
-				});
-			}, responseInterval);
-		}, 1500);
+		// setTimeout(() => {
+		// 	socket.emit("start_typing", { userId });
+		// 	setTimeout(() => {
+				// socket.emit("stop_typing", { userId });
+				// socket.emit("fetch_response", {
+				// 	response: "You said "+message,
+				// 	userId,
+				// });
+		// 	}, responseInterval);
+		// }, 1500);
 	});
 });
